214b7b0391919a43aaa0d00bf03120aa5a5fafdc
Cyrilalt <123979927+Cyrilalt@users.noreply.github.com>
2024-11-05 14:51:47 +0100

    Update NodemailerSettings.js

--- nodemailer/NodemailerSettings.js
+++ nodemailer/NodemailerSettings.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer');
 
 //nodemailer stuff
 const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
     port: 587,
     service: "outlook",
-    secure: false,
     auth: {
         user: process.env.USER,
         pass: process.env.PASS
@@ -15,5 +15,6 @@ const transporter = nodemailer.createTransport({
         ciphers: 'SSLv3'
     }
 });
+
 
 module.exports = transporter;
